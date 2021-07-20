034fbc5101816ae89437a9a7000e5af0f2b3d815
ligialves <ligia.analista@gmail.com>
2021-07-19 22:27:45 -0300

    Initial commit

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,38 @@
+// - Pacote
+package intro;
+
+// - Referências as bibliotecas
+
+
+public class Medidas {
+// - Atributos características
+
+
+    // Metodos e Funções
+    public static void main(String[] args){
+
+        System.out.println("IMPRIMIR CALCULO DE ÁREAS");
+
+
+
+        public void  calcularAreaModoExtenso(){
+            // Calculo de área - Exemplo: tamanho do tapete ou do piso
+            int largura;
+            int comprimento;
+            int resultado;
+
+            largura = 4;
+            comprimento = 3;
+            resultado = largura * comprimento;
+            System.out.println("Total: " + resultado + "m²");
+
+        }
+
+        public void calcularAreaModoCompacto(){
+        // Calculo reduzido
+        int largura = 2;
+        int comprimento = 3;
+        System.out.println("O total da área é: " + largura * comprimento + "m²");
+
+        }
+}
